eb48165cf52731077d92e762699c087478be6264
Aditya Singh <singhaditya6774@gmail.com>
2022-06-23 15:14:26 +0000

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,16 @@
+
+var numberofDownloadButtons=document.querySelectorAll(".link").length;
+for (var i=0; i<numberofDownloadButtons; i++){
+  document.querySelectorAll(".link")[i].addEventListener("click", function (){
+    alert("This feature will be availble soon!!");
+   
+  });
+}
+
+var numberofDownloadButtons=document.querySelectorAll(".button").length;
+for (var i=0; i<numberofDownloadButtons; i++){
+  document.querySelectorAll(".button")[i].addEventListener("click", function (){
+    alert("This feature will be availble soon!!");
+   
+  });
+}
